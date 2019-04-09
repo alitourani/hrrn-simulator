00a5ed17f3912cee1560bec2b00506fea63db97d
alitourani <a.tourani1991@gmail.com>
2019-04-10 00:32:41 +0430

    implementation files

---
+++ OS/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace OS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 20);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(228, 316);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button1.Location = new System.Drawing.Point(291, 225);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(278, 51);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(481, 47);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(291, 47);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(184, 20);
+            this.textBox4.TabIndex = 5;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(291, 113);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(184, 20);
+            this.textBox5.TabIndex = 7;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(481, 110);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Browse";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(291, 179);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(184, 20);
+            this.textBox6.TabIndex = 9;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(481, 176);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Browse";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(501, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "1st File Directory";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(501, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "2nd File Directory";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(501, 160);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "3rd File Directory";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Cambria", 7F);
+            this.label4.Location = new System.Drawing.Point(455, 327);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(135, 12);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "https://github.com/alitourani";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(598, 348);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "HRRN";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ OS/Form1.cs
@@ -0,0 +1,317 @@
+﻿/*
+    Ali Tourani (https://github.com/alitourani/)
+    Operating System - Highest Response Ratio Next (HRRN) Algorithm Simulation
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace OS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            MyProc[0].arrival = MyProc[1].arrival = MyProc[2].arrival = 0;
+            InitializeComponent();
+        }
+
+    const int MAX1 = 4;
+    public struct Proc
+    {
+        public int id;
+        public int arrival;
+        public int burst;
+        public int start;
+        public int rem;
+        public int wait;
+        public int finish;
+        public string NewAdd ;
+        public string Insert_Time;
+        public int turnaround;
+        public float ratio;
+    };
+    public Proc[] MyProc = new Proc[MAX1];
+    public int no;
+    public int[] Pressed = {0,0,0};
+    public string[] timer = new string[3];
+    public string[] Add = {"","",""};
+    public string[] Output = { "", "", "" };
+    public bool mode = true;
+    public int[] sort = {0,0,0};
+    public int[] tmp_nmbr = { 0, 0, 0 };
+                
+                
+    //_____________________________________________
+    int chkprocess(int s){
+        int i;
+            for (i = 0; i < s; i++)
+            {
+                if (MyProc[i].rem != 0)
+                    return 1;
+            }
+            return 0;
+        }
+
+     //_____________________________________________
+     int nextprocess()
+        {
+            int min, l = 0, i, r;
+            min = 0;
+            for (i = 0; i < no; i++)
+            {
+                if (MyProc[i].rem != 0)
+                {
+                    r = ((MyProc[i].wait + MyProc[i].burst) / MyProc[i].burst);
+                    if (r > min)
+                    {
+                        min = r;
+                        l = i;
+                    }
+                }
+            }
+            return l;
+        }
+
+     void Burst_Load(string Addr, int i)
+     {
+         if (Addr == "") {
+             MyProc[i].burst = MyProc[i].arrival;
+             Output[i] = "";
+         }
+         else
+         {
+             int counter = 0;
+             string line="";
+             Addr = Addr.Replace("\\","\\\\");
+             System.IO.StreamReader file = new System.IO.StreamReader(Addr);
+             while ((line = file.ReadLine()) != null)
+             {
+                 Output[i] += line;
+                 counter++;
+             }
+             file.Close();
+             MyProc[i].burst = MyProc[i].arrival + counter;
+         }
+     }
+
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            int i,j,time,n=3;
+            time=0;
+            
+            if (mode == true)
+            {
+                button1.Text = "Restart";
+                button1.ForeColor = Color.Red;
+                button2.Enabled = button3.Enabled = button4.Enabled = false;
+                mode = false;
+                Proc temp;
+                int x;
+
+                for (i = 0; i < n; i++) { 
+                    if (Pressed[i] == 0)
+                    {
+                        MyProc[i].arrival = 0;
+                        MyProc[i].burst = 0;
+                    }
+                }
+
+                for (i = 0; i < n; i++)
+                {
+                    MyProc[i].id = i;
+                    Burst_Load(Add[i], i);
+                    MyProc[i].rem = MyProc[i].burst;
+                    
+                }
+
+                for (i = 0; i <= 2; i++)
+                    if (Pressed[i] == 0) {
+                        Add[i] = "";
+                    }
+
+                for (i = 0; i <= 2; i++)
+                {
+                    MyProc[i].NewAdd = Output[i];
+                    MyProc[i].Insert_Time = timer[i];
+                }
+                    
+                    
+                // Sorting
+                for (i = 0; i <= n-1; i++)
+                {
+                    for (j = i + 1; j < n; j++)
+                    {
+                        if (MyProc[i].arrival > MyProc[j].arrival)
+                        {
+                            temp = MyProc[i];
+                            MyProc[i] = MyProc[j];
+                            MyProc[j] = temp;
+                        }
+                    }
+                }
+
+                no = 0;
+                j = 1;
+
+                MyProc[1].start = MyProc[1].arrival;
+                while (chkprocess(2) == 1)
+                {
+                    if (MyProc[no + 1].arrival == time)
+                        no++;
+
+                    if (MyProc[j].rem != 0)
+                    {
+                        MyProc[j].rem--;
+                        for (i = 1; i <= no; i++)
+                        {
+                            if ((i != j) && (MyProc[i].rem != 0))
+                                MyProc[i].wait++;
+                        }
+                    }
+
+                    else
+                    {
+                        MyProc[j].finish = time;
+                        j = nextprocess();
+                        time--;
+                        MyProc[j].start = time + 1;
+                    }
+                    time++;
+                }
+                MyProc[j].finish = time;
+
+                for (i = 0; i < 3; i++)
+                {
+                    MyProc[i].turnaround = MyProc[i].wait + MyProc[i].burst;
+                    MyProc[i].ratio = (float)MyProc[i].turnaround / (float)MyProc[i].burst;
+                    textBox1.Text += "*File Number : ";
+                    textBox1.Text += (MyProc[i].id + 1);
+                    textBox1.Text += Environment.NewLine; 
+                    textBox1.Text += "*File's Inside : ";
+                    textBox1.Text += Environment.NewLine;
+                    textBox1.Text += MyProc[i].NewAdd;
+                    textBox1.Text += Environment.NewLine;
+                    textBox1.Text += "*Insert Time : ";
+                    textBox1.Text += Environment.NewLine;
+                    textBox1.Text += MyProc[i].Insert_Time;
+                    textBox1.Text += Environment.NewLine; 
+                    textBox1.Text += Environment.NewLine;
+                    textBox1.Text += "*Process Time (millisecond): ";
+                    textBox1.Text += (MyProc[i].burst - MyProc[i].arrival);
+                    textBox1.Text += Environment.NewLine;
+                    textBox1.Text += "*Finish Time : ";
+                    textBox1.Text += MyProc[i].finish;
+                    textBox1.Text += Environment.NewLine;
+                    textBox1.Text += "*Remain Time : ";
+                    textBox1.Text += MyProc[i].rem; 
+                    textBox1.Text += Environment.NewLine;
+                    textBox1.Text += "*Wait Time : ";
+                    textBox1.Text += MyProc[i].wait; 
+                    textBox1.Text += Environment.NewLine;
+                    //textBox1.Text += "*Turn Around Time : ";
+                    //textBox1.Text += MyProc[i].turnaround;
+                    //textBox1.Text += Environment.NewLine;
+                    textBox1.Text += "*Ratio : ";
+                    textBox1.Text += MyProc[i].ratio;
+                    textBox1.Text += Environment.NewLine;
+                    textBox1.Text += "___________________";
+                    textBox1.Text += Environment.NewLine;
+                }
+                no = 0;
+                textBox1.Text += "End of HRRN Management";
+            }
+            else if (mode == false)
+            {
+                button1.Text = "Start";
+                button1.ForeColor = Color.Green;
+                mode = true;
+                textBox1.Text = textBox4.Text = textBox5.Text = textBox6.Text = "";
+                Pressed[0] = Pressed[1] = Pressed[2] = 0;
+                button2.Enabled = button3.Enabled = button4.Enabled = true;
+                MyProc[0].arrival = MyProc[1].arrival = MyProc[2].arrival = 0;
+                MyProc[0].burst = MyProc[1].burst = MyProc[2].burst = 0;
+                timer[0] = timer[1] = timer[2] = "";
+                sort[0] = sort[1] = sort[2] = 0;
+                Add[0] = Add[1] = Add[2] = "";
+                Output[0] = Output[1] = Output[2] = "";
+                time = 0; no = 0;
+            }
+       
+            Pressed[0] = Pressed[1] = Pressed[2] = 0;
+	
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Pressed[0] = 1;
+            int tmp;
+            // Get File
+            OpenFileDialog fdlg = new OpenFileDialog();
+            fdlg.Title = "Choose Your File";
+            fdlg.InitialDirectory = @"";
+            fdlg.Filter = "Text files (*.txt*)|*.*";
+            fdlg.RestoreDirectory = true;
+            if (fdlg.ShowDialog() == DialogResult.OK)
+            {
+                timer[0] = DateTime.Now.ToString();
+                int Mnow = DateTime.Now.Minute;
+                int Snow = DateTime.Now.Second;
+                textBox4.Text = fdlg.FileName;
+                Add[0] = textBox4.Text;
+                tmp = Mnow * 60 + Snow;
+                MyProc[0].arrival = tmp;
+            }
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Pressed[1] = 1;
+            // Get File
+            OpenFileDialog fdlg = new OpenFileDialog();
+            fdlg.Title = "Choose Your File";
+            fdlg.InitialDirectory = @"";
+            fdlg.Filter = "Text files (*.txt*)|*.*";
+            fdlg.RestoreDirectory = true;
+            if (fdlg.ShowDialog() == DialogResult.OK)
+            {
+                timer[1] = DateTime.Now.ToString();
+                int Mnow = DateTime.Now.Minute;
+                int Snow = DateTime.Now.Second;
+                textBox5.Text = fdlg.FileName;
+                Add[1] = textBox5.Text;
+                MyProc[1].arrival = Mnow * 60 + Snow;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Pressed[2] = 1;
+            OpenFileDialog fdlg = new OpenFileDialog();
+            fdlg.Title = "Choose Your File";
+            fdlg.InitialDirectory = @"";
+            fdlg.Filter = "Text files (*.txt*)|*.*";
+            fdlg.RestoreDirectory = true;
+            if (fdlg.ShowDialog() == DialogResult.OK)
+            {
+                timer[2] = DateTime.Now.ToString();
+                int Mnow = DateTime.Now.Minute;
+                int Snow = DateTime.Now.Second;
+                textBox6.Text = fdlg.FileName;
+                Add[2] = textBox6.Text;
+                MyProc[2].arrival = Mnow * 60 + Snow;
+            }
+        }
+
+    }
+}
+
